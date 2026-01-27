4ca3380ab1939c30f0aa94066d255b455303a8aa
Prathamesh Somani <somaniprathamesh017@gmail.com>
2026-01-28 01:00:56 +0530

    Inner Working of react.

---
+++ customReact/customreact.js
@@ -0,0 +1,35 @@
+function customRender (reactElement,Container){
+    /*
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    domElement.setAttribute("href",reactElement.props.href)
+    domElement.setAttribute("target",reactElement.props.target)
+
+    Container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+
+    for (const prop in reactElement.props) {
+       if (prop === "children") continue   //Old way when children was declared in props
+       domElement.setAttribute(prop,reactElement.props[prop])
+    }
+    Container.appendChild(domElement)
+}
+
+
+
+const reactElement = {
+    type: "a",
+    props: {
+        href : "https://www.google.com",
+        target: "_blank"
+    },
+    children : "Click me to visit Google"
+}
+
+const mainContainer = document.querySelector('#root')
+
+
+customRender (reactElement,mainContainer)

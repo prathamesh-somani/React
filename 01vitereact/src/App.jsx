27097fd0b32092f64fc3561edd368084321be72c
Prathamesh Somani <somaniprathamesh017@gmail.com>
2026-01-27 19:58:54 +0530

    Added some internal notes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
 
   return (
-    <>
+    <> 
    <Chai/>
    <h1>Chai aur React | PS </h1>
    <p> Lorem, ipsum dolor.</p>
@@ -14,3 +14,10 @@ function App() {
 }
 
 export default App
+
+
+/* We can only return one html tag in the function , so in order to return more than one tags we wrap them in an <div> wrapper.
+We can also use empty wrappers <> instead of div wrapper.
+*/
+
+// Same Logic applies in React App.
--- 01vitereact/src/chai.jsx
+++ 01vitereact/src/chai.jsx
@@ -6,3 +6,12 @@ function Chai (){
 }
 
 export default Chai
+
+
+
+/* We have created a new function called Chai.
+But in order for function to work and be excuted, it is mandtaory in Vite and React that the function name starts with Capital Letter.
+Also name the file start with capital Letter.
+
+Make sure in Vite if you are returning a HTML Tag in Vite, then the component must be an .jsx file
+*/

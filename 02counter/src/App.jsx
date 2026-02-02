211c798e4b89f36b055c8d4eefdfeafa2ad7839f
Prathamesh Somani <somaniprathamesh017@gmail.com>
2026-02-02 19:38:55 +0530

    interview code

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -46,3 +46,43 @@ export default App
 //Assignment 
 // Create a method so that value never goes below zero in -ve
 // Never goes more than 20
+
+
+// From Interview Perspective
+
+// function App() {
+
+//   const [counter, setCounter]  = useState(15)
+
+//   //let counter = 15
+
+//   const addValue = () => {
+//     //counter = counter + 1
+//     setCounter(prevCounter => prevCounter + 1)
+//     setCounter(prevCounter => prevCounter + 1 )
+//     setCounter(prevCounter => prevCounter + 1)
+//     setCounter(prevCounter => prevCounter + 1)
+    
+//   }
+
+//   const removeValue = () => {
+//     setCounter(counter - 1)
+//   }
+  
+//   return (
+//     <>
+//       <h1>Chai aur react</h1>
+//       <h2>Counter value: {counter}</h2>
+
+//       <button
+//       onClick={addValue}
+//       >Add value {counter}</button> 
+//       <br />
+//       <button
+//       onClick={removeValue}
+//       >remove value {counter}</button>
+//       <p>footer: {counter}</p>
+//     </>
+//   )
+// }
+
